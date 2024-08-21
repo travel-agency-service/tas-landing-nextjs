886d7552f99195319406270e1600dba24b028e55
Parsa Asgari <parsa.asgari3264@gmail.com>
2024-08-21 12:25:21 +0000

    layout-and-content-changes-001

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -12,35 +12,36 @@ export const HeroSection = () => {
     <section className="container w-full">
       <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
         <div className="text-center space-y-8">
-          <Badge variant="outline" className="text-sm py-2">
+          {/* <Badge variant="outline" className="text-sm py-2">
             <span className="mr-2 text-primary">
               <Badge>New</Badge>
             </span>
             <span> Design is out now! </span>
-          </Badge>
+          </Badge> */}
 
           <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
             <h1>
-              Experience the
+            Revolutionizing Visa Applications Through
               <span className="text-transparent px-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
-                Shadcn
+                TAS
               </span>
-              landing page
+              AI
             </h1>
           </div>
 
           <p className="max-w-screen-sm mx-auto text-xl text-muted-foreground">
-            {`We're more than just a tool, we're a community of passionate
-            creators. Get access to exclusive resources, tutorials, and support.`}
+            {`More than just a simple AI.`}
           </p>
 
           <div className="space-y-4 md:space-y-0 md:space-x-4">
+            <Link href="#features">
             <Button className="w-5/6 md:w-1/4 font-bold group/arrow">
               Get Started
               <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
             </Button>
+            </Link>
 
-            <Button
+            {/* <Button
               asChild
               variant="secondary"
               className="w-5/6 md:w-1/4 font-bold"
@@ -51,7 +52,7 @@ export const HeroSection = () => {
               >
                 Github respository
               </Link>
-            </Button>
+            </Button> */}
           </div>
         </div>
 
@@ -63,8 +64,8 @@ export const HeroSection = () => {
             className="w-full md:w-[1200px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center border border-t-2 border-secondary  border-t-primary/30"
             src={
               theme === "light"
-                ? "/hero-image-light.jpeg"
-                : "/hero-image-dark.jpeg"
+                ? "/hero-image-generated.jpeg"
+                : "/hero-image-generated.jpeg"
             }
             alt="dashboard"
           />
